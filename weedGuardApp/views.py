e38eaa8024ba630e85a87ad53e37818ef2735a2d
shemaroger <shemaroger60@gmail.com>
2024-10-21 12:28:12 +0200

    update weedGuard

--- weedGuardApp/views.py
+++ weedGuardApp/views.py
@@ -9,8 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
-# Load the pre-trained model (make sure the path is correct)
-model_path = os.path.join(settings.BASE_DIR, 'D:\Project\WeedGuard\my_trained_model.h5')
+# Load the pre-trained model (ensure the path is correct)
+model_path = os.path.join(settings.BASE_DIR, 'D:\\Project\\WeedGuard\\my_trained_model.keras')  # Update with the new format
 model = load_model(model_path)
 
 # Define image dimensions (must match what you used in training)
@@ -24,16 +24,22 @@ def classify_image(request):
         file_name = default_storage.save(uploaded_file.name, ContentFile(uploaded_file.read()))
         file_path = os.path.join(settings.MEDIA_ROOT, file_name)
         
-        # Preprocess the image for model input
-        img = image.load_img(file_path, target_size=IMAGE_SIZE)
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0  # Normalization (same as in training)
+        try:
+            # Preprocess the image for model input
+            img = image.load_img(file_path, target_size=IMAGE_SIZE)
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array /= 255.0  # Normalization (same as in training)
 
-        # Make prediction
-        predictions = model.predict(img_array)
-        predicted_class = np.argmax(predictions, axis=1)
-        confidence = np.max(predictions)  # Get confidence of the prediction
+            # Make prediction
+            predictions = model.predict(img_array)
+            predicted_class = np.argmax(predictions, axis=1)
+            confidence = np.max(predictions)  # Get confidence of the prediction
+
+        except Exception as e:
+            # Clean up (remove the saved image file after processing)
+            os.remove(file_path)
+            return JsonResponse({'error': str(e)}, status=500)
 
         # Clean up (remove the saved image file after processing)
         os.remove(file_path)
@@ -47,6 +53,4 @@ def classify_image(request):
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def upload_image(request):
-
     return render(request, 'weedGuardApp/upload_image.html')
-
