4bd1fc05ba7bc3193e3c2e3cfef8a974e4951742
shemaroger <shemaroger60@gmail.com>
2024-11-18 22:01:55 +0200

    update

--- weedGuardApp/models.py
+++ weedGuardApp/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+class User(models.Model):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('farmer', 'Farmer'),
+    )
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.fullname} ({self.role})"
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+
 class Prediction(models.Model):
     image = models.ImageField(upload_to='predictions/')
     result = models.CharField(max_length=50)
@@ -10,3 +30,7 @@ class Prediction(models.Model):
 
     def __str__(self):
         return f"{self.result} - {self.timestamp} - {self.site_name or 'Unknown Site'}"
+
+    class Meta:
+        verbose_name = "Prediction"
+        verbose_name_plural = "Predictions"
